49bb4d86545f851d18feebabcc78f11ebe77e6a1
SimonaMitrenova <simona.mitrenova@gmail.com>
2016-10-09 14:00:25 +0300

    Fix question positions

--- js/questions-generator.js
+++ js/questions-generator.js
@@ -2,8 +2,16 @@ function generateQuestions(number, fromWidth, toWidth, fromHeight, toHeight) {
   let questions = [];
 
   for (let index = 0; index < number; index += 1) {
-    let x = getRandomCoordinateValue(fromWidth, toWidth) + 10,
-      y = getRandomCoordinateValue(fromWidth, toWidth) + 10;
+    let x, y;
+    
+    while (true) {
+      x = getRandomCoordinateValue(fromWidth, toWidth) - 40,
+      y = getRandomCoordinateValue(fromWidth, toWidth) - 40;
+
+      if (!questions.some(q => q.x === x && q.y === y)) {
+        break;
+      }
+    }
 
     let currentQuestion = { x: x, y: y };
     questions.push(currentQuestion);
